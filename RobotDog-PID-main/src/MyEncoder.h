1923e53412ad5d47742bce50c6ad2c6ef8360dbf
TMT <105305209+LtMtrunG@users.noreply.github.com>
2024-08-07 12:10:25 +0700

    Update MyEncoder.h

--- RobotDog-PID-main/src/MyEncoder.h
+++ RobotDog-PID-main/src/MyEncoder.h
@@ -1,6 +1,7 @@
+//---------------------------------MyEncoder.h-----------------------------//
 #include <Arduino.h>
 
-#define ENC1_A 4    // MOT 1A
+#define ENC1_A 4    // MOT 1A   
 #define ENC1_B 16   // MOT 1B
 #define ENC_RES 330 // Encoder resolution*Gearbox ratio: 11*30
 
@@ -10,18 +11,23 @@ double th1 = 0;             // Position angle in degrees
 //================================================================================
 void readEncoder1()
 {
+    // Serial.print(cnt1);
     int b = digitalRead(ENC1_B);
     cnt1 = (b > 0) ? (cnt1 + 1) : (cnt1 - 1);
 }
 
 void Init_Encoder()
 {
-    pinMode(ENC1_A, INPUT);
-    pinMode(ENC1_B, INPUT);
+    pinMode(ENC1_A, INPUT_PULLUP);
+    pinMode(ENC1_B, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(ENC1_A), readEncoder1, RISING);
 }
 
 void Get_Angle()
 {
+        // noInterrupts(); // Disable interrupts while accessing shared variable
+
     th1 = cnt1 * 360 / ENC_RES; // Conversion between encoder count and degree
+        // interrupts(); // Re-enable interrupts
+
 }
