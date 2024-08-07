ad4e0253c49895779c29cffcedfe5e88d5736297
TMT <105305209+LtMtrunG@users.noreply.github.com>
2024-08-07 12:11:41 +0700

    Update main.cpp

--- RobotDog-PID-main/src/main.cpp
+++ RobotDog-PID-main/src/main.cpp
@@ -1,41 +1,24 @@
+//---------------------------------Main.cpp-----------------------------//
 #include <Arduino.h>
 #include "MyMotor.h"
 #include "MyEncoder.h"
 #include "MyPID.h"
 #include "MySerial.h"
 bool flag = true;
-
-double input = 360; //INPUT ANGLE HERE
+int count = 0;
 void setup()
 {
   Init_Motor();
   Init_Encoder();
   Init_PID();
   Init_Serial();
-  th1_ref =  input;
 }
 
 void loop()
 {
-  Get_Angle(); 
+  Get_Angle();
+  SerialDataPrint();
+  Run_Motor();
+  SerialDataWrite();
   Compute_PID();
-
-  if (abs(th1) >= th1_ref)
-  {
-    if (flag)
-    {
-      Serial.print("Larger");
-      flag = false;
-        pinMode(MOT1_A, LOW);
-    pinMode(MOT1_B, LOW);
-    }
-    
-  }
-  else
-  {
-    Run_Motor();
-    SerialDataPrint();
-  }
-
-  // SerialDataWrite();
 }
