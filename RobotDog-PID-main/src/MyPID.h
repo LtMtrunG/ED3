1b98a80eb0beb46ab64d8b2a38336df227ab3c74
TMT <105305209+LtMtrunG@users.noreply.github.com>
2024-08-07 12:10:55 +0700

    Update MyPID.h

--- RobotDog-PID-main/src/MyPID.h
+++ RobotDog-PID-main/src/MyPID.h
@@ -1,8 +1,9 @@
+//---------------------------------MyPID.h-----------------------------//
 #include <Arduino.h>
 #include <PID_v1.h>
 
 extern double th1_ref; 
-double kp = 1, ki = 0.1, kd = 0.01;
+double kp = 5, ki = 0.01, kd = 0.01; //official configuration for PID constants
 
 // Input, output, reference
 PID MOT1_PID(&th1, &MOT1_cmd, &th1_ref, kp, ki, kd, DIRECT);
@@ -13,13 +14,16 @@ PID MOT1_PID(&th1, &MOT1_cmd, &th1_ref, kp, ki, kd, DIRECT);
 void Init_PID()
 {
   MOT1_PID.SetMode(AUTOMATIC);
-  MOT1_PID.SetOutputLimits(-110, 110);
+  MOT1_PID.SetOutputLimits(-30, 30);
   MOT1_PID.SetSampleTime(10);
 }
+
 // ================================================================
 void Compute_PID()
 {
   MOT1_PID.SetTunings(kp, ki, kd);
   MOT1_PID.Compute();
+
+  // Add a tolerance to avoid oscillations
 }
 // ================================================================
