622ed2668e12e278d2aa693e2f972f46ed41cddb
TPJamesH <108443772+TPJamesH@users.noreply.github.com>
2024-09-16 15:25:49 +0700

    pid changes

---
+++ ED3-3-motors/ED3-3-motors/RobotDog-PID-main/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/KIM\ TIN\ TRUC/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/KIM\ TIN\ TRUC/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ ED3-3-motors/ED3-3-motors/RobotDog-PID-main/.pio/build/esp32dev/lib7e2/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib7e2/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/KIM\ TIN\ TRUC/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/KIM\ TIN\ TRUC/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/KIM\ TIN\ TRUC/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
